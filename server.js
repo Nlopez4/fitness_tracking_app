4dbc07568e68575b6229ad3b71ecd7bc55633a93
nataliel2514 <natalielopez2514@gmail.com>
2021-07-24 20:10:22 -0500

    add server

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-const express = require('express');
+require("dotenv").config();
+/* ==== External Modules ==== */
+const express = require("express");
+const methodOverride = require("method-override");
 
-const app = express(); 
+/* ==== Internal Modules ==== */
+const routes = require("./routes");
+
+/* ==== Instanced Modules  ==== */
+const app = express();
+
+/* ====  Configuration  ==== */
+const PORT = process.env.PORT || 4000;
+
+app.set("view engine", "ejs");
+app.get('/', function (req, res) {
+    res.send('Welcome');
+  });
+
+app.listen(PORT, () => {
+	console.log(`fitness tracking app is live at http://localhost:${PORT}`);
+});
+
