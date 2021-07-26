ca65a221eb6023d551a6393cb1eb7c708c7c810c
MaricelaDuron0809 <emtdavidian@gmail.com>
2021-07-26 02:01:29 -0700

    log out route

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,33 @@ router.get("/", function (req, res) {
   });
 });
 
+//asking google to take over
 router.get(
     "/auth/google",
     passport.authenticate("google", { scope: ["profile", "email"] })
   );
 
+//this route will give us all the google info we need
+  router.get(
+    "/oauth2callback",
+    passport.authenticate("google", {
+        //if sucessfull this will take us to the workout log
+      successRedirect: "/workoutlog",
+      failureRedirect: "/",
+    })
+  );
+
+//this route logs you out
+router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
   
+
+
+
+
+
+
 
 module.exports = router;
