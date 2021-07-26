7d1314c3e059b160207f4019a25bcacef3dd1ab3
MaricelaDuron0809 <emtdavidian@gmail.com>
2021-07-26 01:50:38 -0700

    deserializeUser Methods

--- auth/passport.js
+++ auth/passport.js
@@ -32,4 +32,21 @@ passport.use(
       }
     )
   );
+
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
   
+
+
+
+
+
