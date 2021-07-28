60e09c8832e53d371600b8a0c9495cec15661e51
nataliel2514 <natalielopez2514@gmail.com>
2021-07-27 20:32:46 -0500

    add config

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true, 
-    useCreateIndex: true,
-    useUnifiedTopology: true 
-});
+// mongoose.connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true, 
+//     useCreateIndex: true,
+//     useUnifiedTopology: true 
+// });
 
-const db = mongoose.connection
-db.once('open', _ => {
-  console.log('Database connected:', process.env.DATABASE_URL)
-})
+// const db = mongoose.connection
+// db.once('open', _ => {
+//   console.log('Database connected:', process.env.DATABASE_URL)
+// })
 
-db.on('error', err => {
-  console.error('connection error:', err)
-})
+// db.on('error', err => {
+//   console.error('connection error:', err)
+// })
